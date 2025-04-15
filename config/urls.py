10574be0bc752f5243bf3c189866eed9e997c620
lucas-jorge <lucasajorge@gmail.com>
2025-04-15 15:45:44 -0300

    Swagger configs

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
-from django.views.generic import RedirectView
+# from django.views.generic import RedirectView  # Unused import removed
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -36,10 +36,10 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Include the capy app URLs under /api/
-    path('api/', include('capy.urls')),  
+    path('api/', include('capy.urls')),
     # --- URLs da Documentação drf-yasg ---
     # Endpoint para o schema JSON bruto
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', 
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
             schema_view.without_ui(cache_timeout=0), name='schema-json'),
     # Endpoint para a interface Swagger UI interativa
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
